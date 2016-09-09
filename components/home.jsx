bcc6e1ed9072a4aff0750708618265c3853520bf
Joe Khoury <khoury@adobe.com>
2016-09-09 10:36:03 -0400

    Add VisitorAPI script on page

--- components/home.jsx
+++ components/home.jsx
@@ -5,13 +5,15 @@ const Home = ({ serverState }) => {
         <html>
             <head>
                 <title>Visitor JS Server Sample APP</title>
-                <link rel="stylesheet" href="/styles.css" />
+                <link rel="stylesheet" href="/css/styles.css" />
             </head>
             <body>
                 <div>
                     <h1>Server Side Rendering!</h1>
                     <h3>Share server state with client: {serverState}</h3>
                 </div>
+
+                <script src="/js/VisitorAPI.js" />
             </body>
         </html>
     );
